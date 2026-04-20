e48be18f087ea594410752537de2d7153a03340d
Sunil Joseph <sunilozhakal@gmail.com>
2026-04-21 00:07:27 +0530

    capstone

--- react/project/ems-backend/src/main/java/com/spg/ems/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spg.ems.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spg.ems.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
